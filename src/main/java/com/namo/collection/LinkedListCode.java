e6f0443e8b947923675d57d94e6b8a37365314e2
wx1458084829 <wx1999125>
2021-12-16 23:37:04 +0800

    完成linkedList的笔记

---
+++ src/main/java/com/namo/collection/LinkedListCode.java
@@ -0,0 +1,18 @@
+package com.namo.collection;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+/**
+ * @author namo
+ * @data 2021/12/16-9:53 PM
+ * @description
+ */
+public class LinkedListCode {
+    @Test
+    public void testLinkedListCode(){
+        LinkedList<Integer> linkedList = new LinkedList<>();
+    }
+}
+
